e3d50c9b2a86cd4f090e6b5fbc5b112372043085
infinityAlive <sonic@ehanlin.com.tw>
2017-08-22 23:46:14 +0800

    modify ad-hswriting

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
 
-
-case "${TRAVIS_TAG}" in
-  *SNAPSHOT* ) 
-    curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://test.ehanlin.com.tw/event/api/Deploy';;
-  *)
-  curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://www.ehanlin.com.tw/event/api/Deploy';;
-esac
+# case "${TRAVIS_TAG}" in
+#   *SNAPSHOT* ) 
+#     curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://test.ehanlin.com.tw/event/api/Deploy';;
+#   *)
+#   curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://www.ehanlin.com.tw/event/api/Deploy';;
+# esac
 
