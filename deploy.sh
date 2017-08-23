3fa4b5e730cf065ca00803f9f8c6697a39e7b51a
infinityAlive <sonic@ehanlin.com.tw>
2017-08-23 19:06:51 +0800

    更新 deploy.sh，依據 tag “SNAPSHOT” 內容部署至不同主機

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
+test="http://test.ehanlin.com.tw/event/api/Deploy"
+production="http://www.ehanlin.com.tw/event/api/Deploy"
+repository="{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}"
+contentTypeJson="Content-Type: application/json"
 
-#case "${TRAVIS_TAG}" in
-#  *SNAPSHOT* ) 
-#    curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://test.ehanlin.com.tw/event/api/Deploy';;
-#  *)
-#  curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://www.ehanlin.com.tw/event/api/Deploy';;
-#esac
-
+case "${TRAVIS_TAG}" in
+*SNAPSHOT*)
+	echo "deploy to test"
+	curl -X POST -H "${contentTypeJson}" -d "${repository}" "${test}"
+	;;
+*)
+	echo "deploy to production"
+	curl -X POST -H "${contentTypeJson}" -d "${repository}" "${production}"
+	;;
+esac
