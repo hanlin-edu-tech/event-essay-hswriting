661a64c8b4261ff4395bc2fc0783adf9465fa445
AsoTiesto <cute17852337569@gmail.com>
2020-02-03 10:46:58 +0800

    調整功能：調整  作文廣告靜態頁面連結錯誤的 btn 並且將專案搬移至 hanlin-edu-tech

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,65 @@
+var Q = require("q");
+var fs = require("fs");
+var del = require("del");
 var gulp = require("gulp");
+var path = require("path");
+var basePath = { base: "src" };
+var readline = require("readline");
+var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
 var pngquant = require("imagemin-pngquant");
 var cleanCSS = require("gulp-clean-css");
-var uglify = require("gulp-uglify");
 var templateUtil = require("gulp-template-util");
-var Q = require("q");
-var path = require("path");
-var fs = require("fs");
-var readline = require("readline");
-var del = require("del");
-var basePath = { base: "src" };
 var dist = "dist";
+
+
+const gcPub = require("gulp-gcloud-publish");
+const bucketNameForTest = "tutor-test-events";
+const bucketNameForProd = "tutor-events";
+const projectId = "tutor-204108";
+const projectIdTest = "tutor-test-238709";
+const keyFileName = "tutor.json";
+const keyFileNameTest = "tutor-test.json";
+const projectName = "event/ad/";
+
+
+let uploadGCSProd = bucketName => {
+  return gulp
+    .src(["dist/*.html", "dist/img/**", "dist/css/**", "dist/js/**"], {
+      base: `${__dirname}/dist/`
+    })
+    .pipe(
+      gcPub({
+        bucket: bucketName,
+        keyFilename: keyFileName,
+        base: projectName,
+        projectId: projectId,
+        public: true,
+        metadata: {
+          cacheControl: "no-store, no-transform"
+        }
+      })
+    );
+};
+
+let uploadGCSTest = bucketName => {
+  return gulp
+    .src(["dist/*.html", "dist/img/**", "dist/css/**", "dist/js/**"], {
+      base: `${__dirname}/dist/`
+    })
+    .pipe(
+      gcPub({
+        bucket: bucketName,
+        keyFilename: keyFileNameTest,
+        base: projectName,
+        projectId: projectIdTest,
+        public: true,
+        metadata: {
+          cacheControl: "no-store, no-transform"
+        }
+      })
+    );
+};
 
 var determineDeployProduction = () => {
   var replaceToProduction = () => {
@@ -23,7 +72,7 @@ var determineDeployProduction = () => {
       };
 
       var changeToCurrent = () => {
-        fs.readFile(entireFilePath, "UTF-8", function(err, data) {
+        fs.readFile(entireFilePath, "UTF-8", function (err, data) {
           if (err) throw err;
           if (data.includes("current.SNAPSHOT")) {
             var fileContent = data.replace(/current\.SNAPSHOT/g, "current");
@@ -48,7 +97,7 @@ var determineDeployProduction = () => {
     output: process.stdout
   });
   var isDeploy;
-  prompts.question("是否部署至正式機 (如果是則輸入 Y) = ", function(input) {
+  prompts.question("是否部署至正式機 (如果是則輸入 Y) = ", function (input) {
     if (input === "Y") {
       replaceToProduction();
     }
@@ -95,7 +144,7 @@ var minifyJS = sourceJS => {
     return gulp
       .src(sourceJS, basePath)
       .pipe(
-        uglify({ mangle: false }).on("error", function(error) {
+        uglify({ mangle: false }).on("error", function (error) {
           console.log(error);
         })
       )
@@ -112,20 +161,23 @@ gulp.task("minifyJS", minifyJS("src/js/*.js"));
 
 gulp.task("package", () => {
   var deferred = Q.defer();
-  Q.fcall(function() {
+  Q.fcall(function () {
     return templateUtil.logPromise(clean(dist));
   })
-    .then(function() {
+    .then(function () {
       return templateUtil.logStream(copyStaticFile());
     })
-    .then(function() {
+    .then(function () {
       return Q.all([
         templateUtil.logStream(minifyImage("src/img/*.png")),
         templateUtil.logStream(minifyCSS("src/css/*.css")),
         templateUtil.logStream(minifyJS("src/js/*.js"))
       ]);
     })
-    .then(function() {
+    .then(function () {
       return templateUtil.logPromise(determineDeployProduction);
     });
 });
+
+gulp.task("uploadGcsTest", uploadGCSTest.bind(uploadGCSTest, bucketNameForTest));
+gulp.task("uploadGcsProd", uploadGCSProd.bind(uploadGCSProd, bucketNameForProd));
